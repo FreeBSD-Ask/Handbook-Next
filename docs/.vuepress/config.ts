ed233fdc79a47c18be9a8c8be7823b5b9ef9ddc6
DasRob3rt <rob3rt.233@gmail.com>
2022-05-17 19:26:23 +0800

    [LICENSE] Change entity name from The FreeBSD Ask Authors to FreeBSD Chinese Community

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,7 +1,7 @@
 /**
  * @file config.ts
  * @brief Configuration of the translated handbook.
- * @copyright Copyright (c) 2022 The FreeBSD Ask Authors. All rights reserved.
+ * @copyright Copyright (c) 2022 FreeBSD Chinese Community. All rights reserved.
  */
 
 import { defaultTheme } from "vuepress";
