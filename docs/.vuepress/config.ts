4493fcc424a2015b53f792fec1d61392af655c8e
ykla <yklaxds@gmail.com>
2022-06-26 11:52:36 +0800

    Update config.ts

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -28,10 +28,10 @@ export default defineUserConfig({
     }),
     sitemapPlugin({
       hostname: 'https://handbook.bsdcn.org',
-        ({
+        {
   modifyTimeGetter: (page) =>
     'fs.statSync(app.dir.source(page.filePathRelative)).mtime.toISOString();'
-}),
+},
     }),
   ],
 });
