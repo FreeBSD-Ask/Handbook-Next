20e1eaceddc0ca84f609b224e13ab3bb2a180318
ykla <yklaxds@gmail.com>
2022-09-22 12:39:55 +0800

    修正 PDF 导出的目录

--- docs/.vuepress/vuepress-pdf.config.ts
+++ docs/.vuepress/vuepress-pdf.config.ts
@@ -4,4 +4,5 @@ import { defineUserConfig } from "@condorhero/vuepress-plugin-export-pdf-v2";
 export default defineUserConfig({
   theme: "@vuepress/theme-default",
   outFile: "FreeBSD-Handbook-CN.pdf",
+  outDir: "docs/.vuepress/dist/",
 });
