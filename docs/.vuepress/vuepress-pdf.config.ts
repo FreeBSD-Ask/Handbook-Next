9d56234924ffdcbc770c73b4404bb57d6c1d76dc
ykla <yklaxds@gmail.com>
2022-09-23 17:07:10 +0800

    测试文件名修改

--- docs/.vuepress/vuepress-pdf.config.ts
+++ docs/.vuepress/vuepress-pdf.config.ts
@@ -3,4 +3,5 @@ import { defineUserConfig } from "@condorhero/vuepress-plugin-export-pdf-v2";
 
 export default defineUserConfig({
   outDir: "docs/.vuepress/dist/",
+  outFile: "FreeBSD-Handbook-YY_MM_DD-HH_mm.pdf",
 });
