a4167ef6e71a408c6a787ce2135707391db8fc25
ykla <yklaxds@gmail.com>
2022-09-23 11:35:27 +0800

    恢复PDF原文件名以便于release

--- docs/.vuepress/vuepress-pdf.config.ts
+++ docs/.vuepress/vuepress-pdf.config.ts
@@ -2,6 +2,5 @@
 import { defineUserConfig } from "@condorhero/vuepress-plugin-export-pdf-v2";
 
 export default defineUserConfig({
-  outFile: "FreeBSD-Handbook-CN.pdf",
   outDir: "docs/.vuepress/dist/",
 });
