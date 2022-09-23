84b8661265c92d20823a09154163de618df13c4b
ykla <yklaxds@gmail.com>
2022-09-23 17:28:23 +0800

    还原

--- docs/.vuepress/vuepress-pdf.config.ts
+++ docs/.vuepress/vuepress-pdf.config.ts
@@ -3,5 +3,4 @@ import { defineUserConfig } from "@condorhero/vuepress-plugin-export-pdf-v2";
 
 export default defineUserConfig({
   outDir: "docs/.vuepress/dist/",
-  outFile: "FreeBSD-Handbook-YY_MM_DD-HH_mm.pdf",
 });
