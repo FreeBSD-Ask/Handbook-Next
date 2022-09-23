1cc49b72fdc5caaaabc9afd0f113c04d3a5a9ae5
ykla <yklaxds@gmail.com>
2022-09-23 19:35:44 +0800

    test

--- docs/.vuepress/vuepress-pdf.config.ts
+++ docs/.vuepress/vuepress-pdf.config.ts
@@ -3,4 +3,7 @@ import { defineUserConfig } from "@condorhero/vuepress-plugin-export-pdf-v2";
 
 export default defineUserConfig({
   outDir: "docs/.vuepress/dist/",
+  routePatterns: {
+  ["index.html", "/yi-zhe-shuo-ming.html","/freebsd-shou-ce.html","!/404.html"]
+  }
 });
