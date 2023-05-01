5f09d04a9a20baea2eec8f113b0cfaa75b6072d8
ykla <yklaxds@gmail.com>
2023-05-01 18:13:50 +0800

    删除评论区插件以解决bug

--- docs/.vuepress/client.ts
+++
@@ -1,9 +0,0 @@
-import { defineClientConfig } from '@vuepress/client'
-import Layout from "./theme/layouts/Layout.vue";
-
-export default defineClientConfig({
-  layouts: {
-    // we override the default layout to provide comment service
-    Layout,
-  },
-})
